ac7a098a0db459c90633e1206696a83eda4a7d6a
Yasir <yyang@sunrisetelecom.com.cn>
2013-03-17 00:06:53 -0700

    remove
    remove unsed files

--- vendor/plugins/in_place_editor_plus/init.rb
+++
@@ -1,5 +0,0 @@
-require 'in_place_editor'
-require 'form_helper'
-ActionController::Base.send :include, InPlaceEditingPlus
-ActionController::Base.helper InPlaceMacrosHelper
-ActionView::Base.send :include, InPlaceMacrosHelper
