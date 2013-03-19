54862d7d6cfc1eb65c21ad13fbdce22e74f97c16
Yasir <yyang@sunrisetelecom.com.cn>
2013-03-18 18:59:22 -0700

    model
    model

---
+++ model/analyzer.rb
@@ -0,0 +1,8 @@
+class Analyzer
+    def initialize
+        @table_name = "analyzers"
+    end
+    def Analyzer.find(id)
+
+    end
+end
---
+++ test/connect.rb
@@ -0,0 +1,12 @@
+require '../model/base_model'
+class Analyzer < Base
+   @@tables_name = "Analyzers"
+    # To change this template use File | Settings | File Templates.
+end
+
+ana = Analyzer.new
+ana = Analyzer.new
+#conn = Analyzer.get_connect
+#Analyzer.get_field_names(conn)
+#all = conn.find(:all)
+#puts all.inspect
